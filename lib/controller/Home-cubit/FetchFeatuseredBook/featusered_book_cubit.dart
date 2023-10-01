2b65aaa90a86ba0b93e2e3f23783aa92f6585081
Ahmed-Alshobaki <Ahmedzon0598596843@gmail.com>
2023-10-01 23:34:25 +0300

    create FeatuseBookCubit Cubit

--- lib/controller/Home-cubit/FetchFeatuseredBook/featusered_book_cubit.dart
+++ lib/controller/Home-cubit/FetchFeatuseredBook/featusered_book_cubit.dart
@@ -1,9 +1,24 @@
 import 'package:bloc/bloc.dart';
+import 'package:book_app/Featusr/HomeRepo/Repo.dart';
 import 'package:book_app/Featusr/model/model.dart';
 import 'package:meta/meta.dart';
 
 part 'featusered_book_state.dart';
 
-class FeatuseredBookCubit extends Cubit<FeatureBookState> {
-  FeatuseredBookCubit() : super(FeaturedBookInitial());
+class FeatuseBookCubit extends Cubit<FeatureBookState> {
+  FeatuseBookCubit(this.home) : super(FeaturedBookInitial());
+   final HomeRepo home;
+
+   void fetchBook()async{
+     emit(FeaturedBookLoaging());
+    var result = await home.fetchNewBook();
+    result.fold((l) {
+      emit(FeatureBookFailure(l.message));
+    }, (r) {
+      emit(FeatureBookSuccess(r));
+    });
+
+   }
+
+
 }
